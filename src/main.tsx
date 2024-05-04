60b6ca5ce92b1d087d9872771240088aff191932
petruha <refloader@gmail.com>
2024-05-04 11:33:06 +0300

    Install react-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,19 @@ import "./index.css";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 import AntConfigProvider from "./components/AntConfigProvider";
+import { QueryClient, QueryClientProvider } from "react-query";
+import React from "react";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <AntConfigProvider>
-      <App />
-    </AntConfigProvider>
-  </Provider>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <AntConfigProvider>
+          <App />
+        </AntConfigProvider>
+      </Provider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
